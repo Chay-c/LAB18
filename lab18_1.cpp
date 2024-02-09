777be019eaf999ebaf920242f37cb01c84cccb21
Chay <chayangkul_c@cmu.ac.th>
2024-02-09 15:15:18 +0700

    lab18

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 using namespace std;
 
-struct Rect{
+struct R{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(R R1, R R2){
+	
+    double x0 = min(R1.x+R1.w,R2.x+R2.w) - max(R1.x,R2.x);
+	double y0 = min(R1.y,R2.y) - max(R1.y-R1.h,R2.y-R2.h);
+	if(x0 <= 0)
+	return 0;
+	return x0*y0;
 }
